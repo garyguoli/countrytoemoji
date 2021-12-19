b6213f2b3de84fc78e9d67b9e4977f69acdd4f27
garyguo <garyguo@me.com>
2021-12-19 21:19:49 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,4 @@
-// Definitions by: Gary Guo <https://github.com/garyguoli>
-// TypeScript Version: 3.2
-// import {countryToEmoji} from './src/main';
-
-export { countryToEmoji } from "./src/main";
-// export default from './src/main';
-
-declare const countryToEmoji
-export default countryToEmoji
+declare module "countrytoemoji" {
+    export function countryToEmoji({ name, iso }: any);
+  }
+  
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+// Definitions by: Gary Guo <https://github.com/garyguoli>
+// TypeScript Version: 3.2
 export * from "./src/main";
 export { countryToEmoji } from "./src/main";
