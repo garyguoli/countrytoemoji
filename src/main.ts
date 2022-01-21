726c0f0482244324cd941d607bc63d936bb7d769
garyguo <garyguo@me.com>
2022-01-21 15:35:09 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { data } from "./contryList";
 export const countryToEmoji = (country) => {
   const countryiso = (country.iso || "").toUpperCase();
   const countryName = (country.name || "").toUpperCase();
-  const checkCode =(a,b) => countryiso.length === 3?a:b;
-  const res = data.find(({ code,code3, name }) => {
-    if (checkCode(code3,code) === countryiso) return true;
-    if (name === countryName) return true;
+  const res = data.find(({ code, code3, name }) => {
+    if (code === countryiso) return true;
+    if (code3 === countryiso) return true;
+    if (name.toUpperCase() === countryName) return true;
+
     return false;
   });
   return res ? res["emoji"] : "";
 };
-export const countryList = data
+export const countryList = data;
 
 export default countryToEmoji;
